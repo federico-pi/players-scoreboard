e6f5f5db81a6f17ae59a4fd596a9929019955a43
Fred Balzi <balzi.fred@gmail.com>
2021-10-10 21:09:08 +0200

    minor tweaks

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,7 +1,7 @@
 import React, {PureComponent} from 'react';
 import PropTypes from 'prop-types';
 import {Counter} from './Counter.js';
-import Icon from './Icon'; 
+import {Icon} from './Icon'; 
 
 export class Player extends PureComponent {
 
